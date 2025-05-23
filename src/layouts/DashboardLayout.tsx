929f9872566ba1d47659f3f81eb9293b66e3de05
Ceejay Charles <ceejay@ceejaycharles.com>
2025-05-23 17:10:12 -0400

    Add Outlet import to DashboardLayout

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { Link, useNavigate, useLocation, Outlet } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import { 
   Calendar, 
